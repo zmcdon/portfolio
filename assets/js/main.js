5cd06bf847d59af3358b5399b490c41bbffc0576
zmcdon <zmc@zmc-macbookpro.roam.corp.google.com>
2016-10-24 11:26:08 -0700

    first push

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+jQuery(document).ready(function($) {
+    $('.level-bar-inner').css('width', '0');
+    
+    $(window).on('load', function() {
+
+        $('.level-bar-inner').each(function() {
+        
+            var itemWidth = $(this).data('level');
+            
+            $(this).animate({
+                width: itemWidth
+            }, 800);
+            
+        });
+
+    });
+   
+// contact me slide
+    $(".btn-cta-primary").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".content").offset().top
+    }, 1500);
+});
+
+// fixed header
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y > 245) {
+    $('.fixedHeader').fadeIn('slow');
+  } else {
+    $('.fixedHeader').fadeOut('slow');
+  }
+});
+
+
+
+});
