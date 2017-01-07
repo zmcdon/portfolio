57adf20a4ae77a49d07c830046400618b9608374
zmcdon <zmc@zmc-macbookpro.roam.corp.google.com>
2017-01-06 17:39:14 -0800

    ReactJS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,66 @@
-jQuery(document).ready(function($) {
-    $('.level-bar-inner').css('width', '0');
-    
-    $(window).on('load', function() {
+class StoryBox extends React.Component {
+  render() {
+    const now = new Date();
 
-        $('.level-bar-inner').each(function() {
-        
-            var itemWidth = $(this).data('level');
-            
-            $(this).animate({
-                width: itemWidth
-            }, 800);
-            
-        });
 
-    });
-   
-// contact me slide
+    return React.createElement('div', [],
+        [
+          React.createElement('h3', { value: 'Stories' }),
+          React.createElement(
+              'p', 
+              { 
+                className: 'lead', 
+              }, 
+              ["Welcome, current time:" + now.toTimeString()]),
+          React.createElement(
+              'p', 
+              { 
+                className: 'lead-two', 
+              }, 
+              ['time section created with React.JS ']),
+        ]);
+  } 
+}
+
+
+$(document).on('ready', function() 
+  {
+
+    ReactDOM.render (
+        React.createElement(StoryBox),
+        document.getElementById('story-box')
+      );
+
+
+   $('.level-bar-inner').css('width', '0');
+  // contact me slide
     $(".btn-cta-primary").click(function() {
     $('html, body').animate({
         scrollTop: $(".sr-only").offset().top - 87
     }, 1500);
+
+  // fixed header
+  $(document).on('scroll', function() {
+    var y = $(this).scrollTop();
+    if (y > 245) {
+      $('.fixedHeader').fadeIn('slow');
+    } else {
+      $('.fixedHeader').fadeOut('slow');
+    }
+  });
 });
 
-// fixed header
-$(document).scroll(function() {
-  var y = $(this).scrollTop();
-  if (y > 245) {
-    $('.fixedHeader').fadeIn('slow');
-  } else {
-    $('.fixedHeader').fadeOut('slow');
-  }
+$(window).on('load', function() {
+
+    $('.level-bar-inner').each(function() {
+        var itemWidth = $(this).data('level');
+
+        $(this).animate({
+            width: itemWidth,
+        }, 800);
+        
+    });
+
 });
-
-
-// this is for getting screen size!
-
-// function heightDetect() {
-//         $(".background img").css("height", $(window).height());
-//     };
-//     heightDetect();
-//     $(window).resize(function() {
-//         heightDetect();
-//     });
-
-
 
 });
---
+++ assets/js/main.jsx
@@ -0,0 +1,43 @@
+// class Comment extends React.Component{
+//   render() {
+//     return(
+//       <div className='comment'>
+//         <p className='comment-header'>Anne Driod</p>
+//         <p className='comment-body'>
+//           I wanna know what love is...
+//         </p>
+//         <div className='comment-actions'>
+//           <a href='#'>Delete comment</a>
+//          </div>
+//        </div>
+//     );
+//   }
+// }
+
+class StoryBox extends React.Component {
+  render(){
+    const now = new Date();
+
+    return( React.createElement('div', null){
+        React.createElement('h3', 'stories')
+        React.createElement("p", {type: "text", value: "Current time:" {now.toTimeString()}, className:'lead'})
+        console.log('hi1');     
+    }
+      );
+  }
+}
+
+class StoryBox extends React.Component {
+  render(){
+    const now = new Date();
+
+    return( <div>
+            <h3>Stories</h3>
+            <p className='lead'>
+            Current time: {now.toTimeString()}
+            </p>
+            </div>
+            console.log('hi2');  
+      );
+  }
+}
